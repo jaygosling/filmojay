16f0642badd7aade1749819c12601bf1772a9607
Javier Jimenez <javijimenezclemente@gmail.com>
2023-08-25 16:09:26 +0200

    Backend v1.0

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,68 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class Movies(db.Model):
+    movie_id = db.Column(db.Integer, primary_key=True)
+    movie_title = db.Column(db.String(120), unique=True, nullable=False)
+    movie_director = db.Column(db.String(80), unique=False, nullable=False)
+    movie_cast = db.Column(db.String(255), unique=False, nullable=False)
+    movie_runtime = db.Column(db.Integer, unique=False, nullable=False)
+    movie_audio_lang = db.Column(db.String(255), unique=False, nullable=False)
+    movie_subs_lang = db.Column(db.String(255), unique=False, nullable=False)
+    tmdb_id = db.Column(db.Integer, unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<Movies {self.title}>'
 
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
+            "id": self.movie_id,
+            "movie_title": self.movie_title,
+            "movie_director": self.movie_director,
+            "movie_cast": self.movie_cast,
+            "movie_runtime": self.movie_runtime,
+            "movie_audio_lang": self.movie_audio_lang,
+            "movie_subs_lang": self.movie_subs_lang,
+            "tmdb_id": self.tmdb_id
+            # do not serialize the password, its a security breach
+        }
+class Extras(db.Model):
+    extras_id = db.Column(db.Integer, primary_key=True)
+    extras_title = db.Column(db.String(120), unique=True, nullable=False)
+    extras_description = db.Column(db.String(500), unique=False, nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
+    extras_runtime = db.Column(db.Integer, unique=False, nullable=False)
+    extras_audio_lang = db.Column(db.String(255), unique=False, nullable=False)
+    extras_subs_lang = db.Column(db.String(255), unique=False, nullable=False)
+    rel_movie = db.relationship(Movies)
+
+    def __repr__(self):
+        return f'<Extras {self.extras_title}>'
+
+    def serialize(self):
+        return {
+            "id": self.extras_id,
+            "extras_title": self.extras_title,
+            "extras_description": self.extras_description,
+            "extras_runtime": self.extras_runtime,
+            "extras_audio_lang": self.extras_audio_lang,
+            "extras_subs_lang": self.extras_subs_lang,
+            "movie_id": self.movie_id
+            # do not serialize the password, its a security breach
+        }
+
+class Languages(db.Model):
+    lang_id = db.Column(db.Integer, primary_key=True)
+    lang_code = db.Column(db.String(120), unique=True, nullable=False)
+    lang_name = db.Column(db.String(500), unique=False, nullable=False)
+    
+    def __repr__(self):
+        return f'<Extras {self.extras_title}>'
+
+    def serialize(self):
+        return {
+            "lang_id": self.lang_id,
+            "lang_code": self.lang_code,
+            "lang_name": self.lang_name
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, Movies, Extras
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,46 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/movie/<int:id>', methods=['GET'])
+def get_movie(id):
+    print(id)
+    result = Movies.query.filter_by(movie_id=id).first()
+    if result:
+        return result.serialize()
+    else:
+        return jsonify({"mensaje":"Película no encontrada"})
+    
+@api.route('/extras/<int:id>', methods=['GET'])
+def get_extras(id):
+    result = Extras.query.filter_by(movie_id=id).all()
+    result = list(map(lambda x: x.serialize(), result))
+
+    if result:
+        return result
+    else:
+        return jsonify({"mensaje":"Extras no encontrados"})
+
+@api.route('/movie/add', methods=['POST'])
+def add_movie():
+    body = request.get_json()
+    movie_exists = Movies.query.filter_by(tmdb_id=body["tmdb_id"]).first()
+
+    if movie_exists:
+        return jsonify({"mensaje":"Ese ID de TMDB ya existe en la base de datos"})
+    else:
+        movie = Movies(movie_title=body["movie_title"], movie_director=body["movie_director"], movie_runtime=body["movie_runtime"],movie_cast=body["movie_cast"],movie_audio_lang=body["movie_audio_lang"],movie_subs_lang=body["movie_subs_lang"],tmdb_id=body["tmdb_id"])
+        db.session.add(movie)
+        db.session.commit()
+
+        return jsonify({"mensaje": "Película añadida a la base de datos"})
+
+@api.route('/extras/add', methods=['POST'])
+def add_extras():
+    body = request.get_json()
+    extras = Extras(extras_title=body["extras_title"],extras_description=body["extras_description"],extras_runtime=body["extras_runtime"],extras_audio_lang=body["extras_audio_lang"],extras_subs_lang=body["extras_subs_lang"],movie_id=body["movie_id"])
+    db.session.add(extras)
+    db.session.commit()
+
+    return jsonify({"mensaje": "Extra añadido a la base de datos"})
+
